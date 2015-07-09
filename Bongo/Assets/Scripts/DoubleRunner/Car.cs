080f41a1465a04962178df9a834fe42867b662d8
Nachtrind <Kerstin@Nachtrind.de>
2015-07-09 15:23:12 +0200

    New Prefabs
    Including Mysterious Car Textures  :camel:

---
+++ Bongo/Assets/Scripts/DoubleRunner/Car.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Car : MonoBehaviour
+{
+
+	public Material[] carMats;
+
+	// Use this for initialization
+	void Start ()
+	{
+		this.GetComponent<Renderer> ().material = carMats [Random.Range (0, carMats.Length - 1)];
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
