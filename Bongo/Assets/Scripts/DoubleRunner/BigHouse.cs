96014cdfc71b96e45ae2cb7e3cfb4a1b2eff3b42
Nachtrind <Kerstin@Nachtrind.de>
2015-07-10 10:17:27 +0200

    Prefabs, Materials

---
+++ Bongo/Assets/Scripts/DoubleRunner/BigHouse.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BigHouse : MonoBehaviour
+{
+
+	public Material[] mats;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		int ran = Random.Range (0, mats.Length - 1);
+		this.GetComponent<Renderer> ().material = mats [ran];
+		Renderer[] rens = GetComponentsInChildren<Renderer> ();
+
+		foreach (Renderer ren in rens) {
+			ren.material = mats [ran];
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
